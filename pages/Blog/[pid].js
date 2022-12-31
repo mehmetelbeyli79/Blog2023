e823753bf09e000a0be1f92e961156acef036aad
Mehmet Elbeyli <mehmetelbeyli88@gmail.com>
2023-01-01 00:14:00 +0300

    refak1

--- pages/Blog/[pid].js
+++ pages/Blog/[pid].js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Head from "next/head";
 import { allPosts } from "contentlayer/generated";
 import { useRouter } from "next/router";
@@ -6,44 +6,51 @@ import parse from "html-react-parser";
 import { tr } from "date-fns/locale";
 import { format, parseISO } from "date-fns";
 import Link from "next/link";
+
+
 function PostDetay() {
+  
   const router = useRouter();
   const url = router.query.pid;
-  if (router.isFallback) {
-    return <div>Loading...</div>
-  }
-  //console.log(allPosts[0].url)
-  const post = allPosts.filter((p) => p.url === "/Blog/" + url);
-  //console.log(post);
+  const post1 = allPosts.filter((p) => p.url === "/Blog/" + url);
   return (
     <div>
       <Head>
-        <title>{post[0].title ? post[0].title : "" }</title>
-        <meta name="description" content={post[0].title ? post[0].title : ""} />
-        <meta name="keywords" content={post[0].keywords ? post[0].keywords : "" } />
+        <title>{post1[0].title ? post1[0].title : ""}</title>
+        <meta name="description" content={post1[0].title ? post1[0].title : ""} />
+        <meta
+          name="keywords"
+          content={post1[0].keywords ? post1[0].keywords : ""}
+        />
         <meta name="author" content="Mehmet Elbeyli" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="flex flex-col my-16 w-full items-center">
-        
-        {post ? post.map((veri,index) => {
-          return (
-            <div className="flex-col lg:w-2/5 mx-3 lg:mx-0" key={index}>
-              <h2 className="mb-3 text-3xl">{veri.title || null}</h2>
-              <h2 className="mb-3 text-lg text-slate-400">{veri.subtitle || null}</h2>
-              <h2 className="mb-3 text-lg text-slate-300">
-                {format(parseISO(veri.date), "d LLLL yyyy", {
-                  locale: tr,
-                })}
-              </h2>
-              <p className=" leading-8 tracking-normal">
-                {parse(veri.body.html)}
-              </p>
-            </div>
-          );
-        }): ""}
-        <Link href="/Blog" className="text-start text-red-900 mb-5 bg-red-200 p-2 rounded-md">
-          Geri Dön 
+        {post1
+          ? post1.map((veri, index) => {
+              return (
+                <div className="flex-col lg:w-2/5 mx-3 lg:mx-0" key={index}>
+                  <h2 className="mb-3 text-3xl">{veri.title || null}</h2>
+                  <h2 className="mb-3 text-lg text-slate-400">
+                    {veri.subtitle || null}
+                  </h2>
+                  <h2 className="mb-3 text-lg text-slate-300">
+                    {format(parseISO(veri.date), "d LLLL yyyy", {
+                      locale: tr,
+                    })}
+                  </h2>
+                  <p className=" leading-8 tracking-normal">
+                    {parse(veri.body.html)}
+                  </p>
+                </div>
+              );
+            })
+          : ""}
+        <Link
+          href="/Blog"
+          className="text-start text-red-900 mb-5 bg-red-200 p-2 rounded-md"
+        >
+          Geri Dön
         </Link>
       </div>
     </div>
